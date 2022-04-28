4348f20092d22de501a801b653ab8a8e89c60f78
UBN100 <5423410@mail.ru>
2022-04-28 15:23:52 +0300

    CrimeLab

--- app/src/main/java/com/mygeekbranch/criminalintent_32/CrimeActivity.java
+++ app/src/main/java/com/mygeekbranch/criminalintent_32/CrimeActivity.java
@@ -6,20 +6,11 @@ import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends SingleFragmentActivity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_conteiner);
-        if (fragment == null){
-            fragment = new CrimeFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragment_conteiner,fragment)
-                    .commit();
-
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
